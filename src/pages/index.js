92a5145c1da47e1e45692542332d617acf2d9814
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-10-06 09:03:04 -0300

    Update SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,38 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Simplifi.ga</title>
         <link rel="shortcut icon" href="/favicon.png"/>
-        <meta name="description" content="Simplifique seu link com Simplifi.ga" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <meta name="google-site-verification" content="QrL3AgdnXVfVbQtBBH0KSaKerIN1CGVcZ-78luRQdl4" />
         <link href="https://fonts.googleapis.com/css2?family=Sarala&display=swap" rel="stylesheet" />
+     
+        { /* SEO TAG */ }
+
+        <title>Simplifiga</title>
+        <meta name="title" content="Simplifiga" />
+        <meta name="description" content="Encurtador e Simplificador de URLs para torná-las memoráveis." />
+        <meta name="robots" content="index, follow"/>
+        <link rel="canonical" href="https://simplifi.ga/" />
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://simplifi.ga/"/>
+        <meta property="og:title" content="Simplifiga"/>
+        <meta property="og:description" content="Encurtador e Simplificador de URLs para torná-las memoráveis."/>
+        <meta property="og:image" content="https://raw.githubusercontent.com/HugoRodriguesQW/simplifiga/main/banner.png"/>
+        <meta property="og:url" content="https://simplifi.ga/" />
+        <meta property="og:site_name" content="Simplifiga" />
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://simplifi.ga/"/>
+        <meta property="twitter:title" content="Simplifiga"/>
+        <meta property="twitter:description" content="Encurtador e Simplificador de URLs para torná-las memoráveis."/>
+        <meta property="twitter:image" content="https://raw.githubusercontent.com/HugoRodriguesQW/simplifiga/main/banner.png"/>
       </Head>
 
       <h1 className={styles.titleLogoImage}>
-        <img src="/favicon.png" alt="Simplifi.ga"/>
+        <img src="/favicon.png" loading="lazy" alt="Simplifi.ga" title="Simplifiga"/>
         <span>Simplifi.ga</span>
       </h1>
 
