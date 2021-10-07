364c9336bfcf9cbd3e71ad3d20871605b04926d3
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-10-06 22:40:43 -0300

    Fixxx sitemap

---
+++ src/pages/api/my-sitemap.js
@@ -0,0 +1,25 @@
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+
+const handler = async (req, res) => {
+
+  const links = [
+    { url: "/[redirect]", changefreq: "daily", priority: 1 },
+    { url: "/index", changefreq: "daily", priority: 1 },
+  ];
+
+
+  const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
+
+  res.writeHead(200, {
+    "Content-Type": "application/xml",
+  });
+
+  const xmlString = await streamToPromise(
+    Readable.from(links).pipe(stream)
+  ).then((data) => data.toString());
+
+  res.end(xmlString);
+};
+
+export default handler
