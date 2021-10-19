31f0f8eff48fbb2afbca66b13a6b66a439c5ab63
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-10-19 13:22:38 -0300

    Icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <link href="https://fonts.googleapis.com/css2?family=Sarala&display=swap" rel="stylesheet" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <link rel="shortcut icon" href="/favicon.png"type="image/png"/>
+      <link rel="shortcut icon" href="/favicon.ico"type="image/x-icon"/>
       <link rel="alternate" type="application/xml" title="sitemap" href="https://simplifi.ga/api/my-sitemap" />
       
       <meta name="google-site-verification" content="QrL3AgdnXVfVbQtBBH0KSaKerIN1CGVcZ-78luRQdl4" />
