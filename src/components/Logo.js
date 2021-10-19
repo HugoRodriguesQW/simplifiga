bfae393c5cb92f8e579091d192a057b58cb04835
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-10-19 12:53:23 -0300

    New logo & colors

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,8 +4,16 @@ import styles from '../styles/components/Logo.module.css'
 export function Logo({type}) {
   return (
     <h1 className={styles[`${type}LogoImage`]}>
-        <img src="/favicon.png" loading="lazy" alt="Simplifi.ga" title="Simplifiga"/>
-        <span>Simplifi.ga</span>
+       {
+         type === "full" ? (
+          <>
+          <img src="/Logo512x512.svg" loading="lazy" alt="Simplifi.ga" title="Simplifiga"/>
+          <span>Simplifi.ga</span>
+          </>
+         ): (
+          <img src="/Logo2547x512.svg" loading="lazy" alt="Simplifi.ga" title="Simplifiga"/>
+         )
+       }
     </h1>
   )
 }
