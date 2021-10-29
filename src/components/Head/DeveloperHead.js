64af09afd2691515a1aabe237d4e78bd4f35d59f
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-10-28 23:48:28 -0300

    Layout - Developer - API - page

---
+++ src/components/Head/DeveloperHead.js
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/next-script-for-ga */
+
+import Head from 'next/head'
+
+export function DeveloperHead() {
+  return (
+    <Head>
+      <title>API | Simplifiga</title>
+
+      {/* SEO - ROBOTS */}
+      <link rel="canonical" href="https://simplifi.ga/developer/" />
+      <meta name="title" content="API | Simplifiga" />
+      <meta name="description" content="Funcionamento da API da Simplifiga e métodos de utilização" />
+      <meta name="robots" content="index, follow"/>
+    </Head>
+  )
+}
--- src/pages/developer.js
+++ src/pages/developer.js
@@ -1,14 +1,61 @@
 import { Header } from "../components/Header";
+import styles from '../styles/pages/Developer.module.css'
+import { useEffect, useState } from "react";
+import { DeveloperHead } from "../components/Head/DeveloperHead";
 
 export default function Developer({logged}) {
+
+
+  const indexes = [['introduction', 'Introdução'], ['usage', 'Utilização']]
+  const [currentIndex, setCurrentIndex] = useState()
+
+  useEffect(()=> {
+    document.addEventListener('scroll', ()=> {
+      let current = indexes[0][0]
+      let scrolled = document.scrollingElement.scrollTop + 60
+      indexes.map((ind)=> {
+        const offset = document.getElementById(ind[0])?.offsetTop
+        if(!offset) return {id: "", is: false}
+        return {id: ind[0], is: offset  <= scrolled}
+      }).forEach((ind)=> {
+        if(ind.is) current = ind.id
+      })
+      if(current === currentIndex) return
+      setTimeout(()=> {
+      setCurrentIndex(current)}, 200)
+    }) 
+  }, [])
+  
   return (
     <>
+    <DeveloperHead/>
      { logged ? (
       <Header fixed padding routes={['/dashboard', '/developer', '/','Sair']}/>
     ) : (
       <Header fixed padding routes={[ '/user/register', '/user/login', '/']}/>
     )
     }
+
+    <div className={styles.developerContainer}>
+      <aside className={styles.menu}>
+        {
+          indexes.map((keys, index)=> {
+            return (
+              <a 
+              key={keys[0]+index} className={keys[0] === currentIndex? styles.currentIndex : null}
+              onClick={()=> {setCurrentIndex(keys[0])}} href={`#${keys[0]}`}>{keys[1]}
+              </a>
+            )
+          })
+        }
+      </aside>
+      <div className={styles.developerContent}>
+        <div>
+        <h3 id="introduction" >A API do Simplifiga</h3>
+        <p>Este é um parágrafo Este é um parágrEste é um parágrafo Este é um parágrafo Este é um parágrafo Este é um parágrafo Este é um parágrafo Este é um parágrafo</p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
