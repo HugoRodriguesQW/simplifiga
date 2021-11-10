2e11e68fd41175090b61d62bc371dbd875f187e9
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-11-09 23:33:10 -0300

    Restore missing

--- src/pages/dashboard/links.js
+++ src/pages/dashboard/links.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { SideBar } from "../../components/Dashboard/SideBar";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
