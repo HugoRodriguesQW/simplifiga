04fbf341e58a66457ce93dd663f7e598d5fca3c8
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-11-10 11:35:27 -0300

    OAuth2 && gmail service

--- src/pages/api/send-code.js
+++ src/pages/api/send-code.js
@@ -2,6 +2,8 @@ import NextCors from "nextjs-cors"
 import { ResetTools } from "../../utils/reset"
 import {Database} from './database'
 import nodemailer from 'nodemailer'
+import {google} from 'googleapis'
+
 
 const handler = async (req, res) => {
 
@@ -27,23 +29,32 @@ const handler = async (req, res) => {
  
   if(!code) return res.status(400).json(onError(res, 400))
 
+  const oAuth2 = new google.auth.OAuth2(process.env.G_ID, process.env.G_SECRET, process.env.REDIRECT_TO)
+  oAuth2.setCredentials({refresh_token: process.env.R_TOKEN})
+
+  const acessToken = await oAuth2.getAccessToken()
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-    user: process.env.APP_EMAIL,
-    pass: process.env.APP_EMAIL_PASS
+      type: 'OAuth2',
+      user: process.env.APP_EMAIL,
+      clientId: process.env.G_ID,
+      clientSecret: process.env.G_SECRET,
+      refreshToken: process.env.R_TOKEN,
+      accessToken: acessToken
     }
     });
 
   const mailOptions = {
-    from: 'mailvitorhugosr@gmail.com',
+    from: 'SIMPLIFIGA <mailvitorhugosr@gmail.com>',
     to: email,
     subject: '[Simplifiga] Recuperação de conta',
-    text: `Se não foi você que requisitou verifique sua conta. Este é o código de recuperação de senha: ${code}. Atenção: este código expira em 10 minutos.`
+    text: `Se não foi você que requisitou verifique sua conta. Este é o código de recuperação de senha: ${code}. Atenção: este código expira em 10 minutos.`,
+    html: `<h1>Se não foi você que requisitou verifique sua conta. Este é o código de recuperação de senha: ${code}. Atenção: este código expira em 10 minutos.</h1>`
   };
   
-  console.log("Log: send-code:", email)
-
+  
   transporter.sendMail(mailOptions, (err, info) => {
     if(err) {
       console.log("Error: send-code:", err)
