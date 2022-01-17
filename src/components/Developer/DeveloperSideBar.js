d0305209fe8ffe5e4698cd0cd3bb4eeb2a97e03b
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2022-01-17 10:21:14 -0300

    Fix build error

--- src/components/Developer/DeveloperSideBar.js
+++ src/components/Developer/DeveloperSideBar.js
@@ -1,45 +1,55 @@
-import { useEffect, useState } from "react"
-import styles from '../../styles/components/Developer/DeveloperSideBar.module.css'
+import { useEffect, useState } from "react";
+import styles from "../../styles/components/developer/DeveloperSideBar.module.css";
 
-export function DeveloperSideBar({content}) {
+export function DeveloperSideBar({ content }) {
+  const indexes = content.document
+    .map((elem) => {
+      return elem.anchor;
+    })
+    .filter((value) => {
+      return value != null;
+    });
 
-  const indexes = content.document.map((elem)=> {
-    return elem.anchor
-  }).filter((value)=> { return value != null})
+  const [currentIndex, setCurrentIndex] = useState();
 
-  const [currentIndex, setCurrentIndex] = useState()
-
-  useEffect(()=> {
-    document.addEventListener('load', setCurrentIndex(indexes[0][0]))
-    document.addEventListener('scroll', ()=> {
-      let current = indexes[0][0]
-      let scrolled = document.scrollingElement.scrollTop + 60
-      indexes.map((ind)=> {
-        const offset = document.getElementById(ind[0])?.offsetTop
-        if(!offset) return {id: "", is: false}
-        return {id: ind[0], is: offset  <= scrolled}
-      }).forEach((ind)=> {
-        if(ind.is) current = ind.id
-      })
-      if(current === currentIndex) return
-      setTimeout(()=> {
-      setCurrentIndex(current)}, 200)
-    }) 
-  }, [])
+  useEffect(() => {
+    document.addEventListener("load", setCurrentIndex(indexes[0][0]));
+    document.addEventListener("scroll", () => {
+      let current = indexes[0][0];
+      let scrolled = document.scrollingElement.scrollTop + 60;
+      indexes
+        .map((ind) => {
+          const offset = document.getElementById(ind[0])?.offsetTop;
+          if (!offset) return { id: "", is: false };
+          return { id: ind[0], is: offset <= scrolled };
+        })
+        .forEach((ind) => {
+          if (ind.is) current = ind.id;
+        });
+      if (current === currentIndex) return;
+      setTimeout(() => {
+        setCurrentIndex(current);
+      }, 200);
+    });
+  }, []);
 
   return (
     <aside className={styles.menu}>
-        {
-          indexes.map((keys, index)=> {
-            const mykey = keys[0]
-            return (
-              <a 
-              key={mykey+index} className={mykey === currentIndex? styles.currentIndex : null}
-              onClick={()=> {setCurrentIndex(mykey)}} href={`#${mykey}`}>{keys[1]}
-              </a>
-            )
-          })
-        }
-      </aside>
-  )
+      {indexes.map((keys, index) => {
+        const mykey = keys[0];
+        return (
+          <a
+            key={mykey + index}
+            className={mykey === currentIndex ? styles.currentIndex : null}
+            onClick={() => {
+              setCurrentIndex(mykey);
+            }}
+            href={`#${mykey}`}
+          >
+            {keys[1]}
+          </a>
+        );
+      })}
+    </aside>
+  );
 }
