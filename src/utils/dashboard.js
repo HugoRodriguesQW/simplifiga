e289f2e7f567bffcd2be5e24fd94f071d754f171
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2022-01-24 13:58:38 -0300

    Regions counter

--- src/utils/dashboard.js
+++ src/utils/dashboard.js
@@ -15,7 +15,8 @@ export function filterReferences(links) {
 
 export function filterLocations(links) {
   const locations = links && links.map((link) => link.locations);
-  return locations.flatMap((element) => [...element]);
+  const resLoc = locations.flatMap((element) => [...element]);
+  return sumArrayLocations(resLoc);
 }
 
 export async function getAllDataFromAPI([token, uri], callback) {
@@ -31,4 +32,43 @@ export async function getAllDataFromAPI([token, uri], callback) {
     const data = JSON.parse(decrypted);
     if (data.length > 0) callback(data);
   });
+}
+
+function sumArrayLocations(locations) {
+  const countries = [];
+  locations.forEach(({ country, code }) => {
+    if (countries.filter((c) => c.country === country).length !== 0) return;
+
+    countries.push(
+      locations
+        .filter((c) => c.country === country)
+        .reduce((a, b) => {
+          const regions = b.regions.concat(a.regions);
+          const resRegions = [];
+          regions.forEach(({ name }) => {
+            console.info("Resolving:", name);
+            if (resRegions.filter((c) => c.name === name).length !== 0) return;
+            resRegions.push(
+              regions
+                .filter((c) => c.name === name)
+                .reduce((c, d) => {
+                  return {
+                    name,
+                    clicks: c.clicks + d.clicks,
+                  };
+                })
+            );
+          });
+
+          return {
+            country,
+            code,
+            regions: resRegions,
+          };
+        })
+    );
+  });
+
+  console.info(countries[0]);
+  return countries;
 }
